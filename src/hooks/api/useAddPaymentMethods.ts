39077312611db0e1c880d313fa7f51c975a4d12e
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-09-27 14:30:38 +0900

    :recycle: refactor: useAddPaymentMethods optimistic으로 변경

--- src/hooks/api/useAddPaymentMethods.ts
+++ src/hooks/api/useAddPaymentMethods.ts
@@ -1,4 +1,5 @@
 import { paymentMethodsService } from '@/service/paymentMethodsService';
+import { Card, CreateCardRequest } from '@/types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export function useAddPaymentMethods() {
@@ -6,7 +7,27 @@ export function useAddPaymentMethods() {
 
   return useMutation({
     mutationFn: paymentMethodsService.createCard,
-    onSuccess: () => {
+    onMutate: async (newData: CreateCardRequest) => {
+      await queryClient.cancelQueries({ queryKey: ['/api/payment-methods'] });
+      const previousData = queryClient.getQueryData(['/api/payment-methods']);
+
+      queryClient.setQueryData(['/api/payment-methods'], (old: { data: Card[] } | undefined) => {
+        if (old == null) {
+          return { data: [newData] };
+        }
+
+        return {
+          ...old,
+          data: [...old.data, newData],
+        };
+      });
+
+      return { previousData };
+    },
+    onError: (_error, _newData, onMutateResult) => {
+      queryClient.setQueryData(['/api/payment-methods'], onMutateResult?.previousData);
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['/api/payment-methods'] });
     },
   });
